94051f89e066c137e478d9e43494e3b40072901a
Chris Beddome <chrisbeddome@gmail.com>
2022-06-21 16:04:19 -0400

    can now set port and host in .env.local

---
+++ bin/dev.js
@@ -0,0 +1,21 @@
+const { exec } = require('child_process');
+const path = require('path');
+const dotenv = require('dotenv')
+
+dotenv.config({path: path.join(__dirname, "../", ".env.local")});
+
+const PORT = parseInt(process.env.PORT);
+const HOSTNAME = process.env.HOSTNAME;
+
+if (!PORT) {
+  throw new Error('Please add PORT to .env.local');
+}
+if (!HOSTNAME) {
+  throw new Error('Please add HOSTNAME to .env.local');
+}
+
+const serverProcess = exec(path.resolve(__dirname, "../", `node_modules/next/dist/bin/next dev -p ${PORT} -H ${HOSTNAME}`))
+
+serverProcess.stdout.on('data', data => {
+  console.log(data); 
+});
