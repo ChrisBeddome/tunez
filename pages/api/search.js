a68df885f60684a5f35db933908af54250214a34
Chris Beddome <chrisbeddome@gmail.com>
2022-06-30 12:45:40 -0400

    refactor

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,46 +2,52 @@ import connectToDatabase from "/lib/connectToDatabase";
 
 export default async function handler(req, res) {
   const { query } = req.query;
-  let results;
-
-  if (query) {
-    const { db } = await connectToDatabase();
-    results = {
-      products: (
-        await db
-          .collection("products")
-          .find({ name: { $regex: query, $options: "i" } })
-          .limit(10)
-          .toArray()
-      ).map((product) => {
-        return {
-          _id: product._id,
-          name: product.name,
-          thumbnailUrl: product.imageUrl,
-          link: `/shop/products/${product._id}`
-        };
-      }),
-      categories: (
-        await db
-          .collection("categories")
-          .find({ name: { $regex: query, $options: "i" } })
-          .limit(5)
-          .toArray()
-      ).map((category) => {
-        return {
-          _id: category._id,
-          name: category.name,
-          thumbnailUrl: category.iconUrl,
-          link: `/shop/categories/${category.slug}`
-        };
-      }),
-    };
-  } else {
-    results = {
-      products: [],
-      categories: [],
-    };
-  }
-
+  const results = await getSearchResults(query);
   res.end(JSON.stringify({ results }));
+}
+
+async function getSearchResults(query) {
+  const { db } = query && (await connectToDatabase());
+  return {
+    products: query
+      ? (await searchProducts(query, db)).map(serializeProduct)
+      : [],
+    categories: query
+      ? (await searchCategories(query, db)).map(serializeCategory)
+      : [],
+  };
+}
+
+async function searchProducts(query, db) {
+  return await db
+    .collection("products")
+    .find({ name: { $regex: query, $options: "i" } })
+    .limit(10)
+    .toArray();
+}
+
+async function searchCategories(query, db) {
+  return await db
+    .collection("categories")
+    .find({ name: { $regex: query, $options: "i" } })
+    .limit(5)
+    .toArray();
+}
+
+function serializeProduct(product) {
+  return {
+    _id: product._id,
+    name: product.name,
+    thumbnailUrl: product.imageUrl,
+    link: `/shop/products/${product._id}`,
+  };
+}
+
+function serializeCategory(category) {
+  return {
+    _id: category._id,
+    name: category.name,
+    thumbnailUrl: category.iconUrl,
+    link: `/shop/categories/${category.slug}`,
+  };
 }
